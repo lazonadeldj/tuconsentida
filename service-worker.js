fda6cbd094a97f6b2a4c1525d5e7942cabf2724c
lazonadeldj <rolimixdj20@gmail.com>
2025-09-10 12:31:14 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'la-zona-del-dj-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/la_zona_del_dj_PROYECTO_BASE64.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
